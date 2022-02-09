10ffc5b845ca177f3393c4e5faaa2972955ab2f0
Jeffrey Voltaire <Jeffreyv300@gmail.com>
2022-02-09 08:38:31 -0500

    Initial Commit

---
+++ SeniorCapstone(2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeniorCapstone(2)
+//
+//  Created by Jeffrey Voltaire on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
